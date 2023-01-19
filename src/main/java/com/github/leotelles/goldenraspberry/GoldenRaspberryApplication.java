7a0f3fd52ce2d26b4da4de0fb81ec365869c27d8
Leonardo Telles <leo.c.telles@gmail.com>
2023-01-19 08:09:25 -0300

    first commit

---
+++ src/main/java/com/github/leotelles/goldenraspberry/GoldenRaspberryApplication.java
@@ -0,0 +1,13 @@
+package com.github.leotelles.goldenraspberry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoldenRaspberryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoldenRaspberryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/leotelles/goldenraspberry/GoldenRaspberryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.leotelles.goldenraspberry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoldenRaspberryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
