88e499e692f3c5bf584a563bb7f9ae3e90178557
Leonardo Telles <leo.c.telles@gmail.com>
2023-01-19 08:51:07 -0300

    create model and repository

---
+++ src/main/java/com/github/leotelles/goldenraspberry/domain/AwardModel.java
@@ -0,0 +1,26 @@
+package com.github.leotelles.goldenraspberry.domain;
+
+import lombok.Data;
+import org.springframework.lang.Nullable;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+public class AwardModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @Column(name = "release_year")
+    private String year;
+
+    private String title;
+    private String studios;
+    private String producers;
+
+    @Nullable
+    private String winner;
+
+}
